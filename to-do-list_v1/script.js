af7bbb782f3ae4432a27376df405549cfb1ef3f1
dahrif <dahri.fatima.dev@gmail.com>
2021-06-22 15:45:19 +0100

    realisation

--- to-do-list_v1/script.js
+++ to-do-list_v1/script.js
@@ -1,19 +1,17 @@
 // Composant : Tâche
-class Task extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-  
+class Tache extends React.Component {
+   
+
   render() {
-    let class_name = 'task'
-    class_name += this.props.done ? ' task-success' : ' task-info';
-  
-    return (
-      <div className={class_name}>
-        <span>{this.props.value}</span>
-        <i className="close">&times;</i>
-      </div>
-    )
+      let class_name = 'tache'
+      class_name += this.props.done ? ' tache-faite' : ' tache-info';
+
+      return (
+          <div className={class_name}>
+              <span>{this.props.value}</span>
+              <i className="close">&times;</i>
+          </div>
+      )
   }
 }
 
@@ -24,7 +22,7 @@ class App extends React.Component {
     super(props)
 
     this.state = {
-       taskList: []
+       tacheList: []
     };
   }
   componentDidMount() {
@@ -33,10 +31,12 @@ class App extends React.Component {
   chargementDonnees(){
 
     var dataList = null;
+    
     // Chargement de données par Ajax
+
     $.getJSON( "data.json", 
     function( data ) {
-      this.setState({ taskList: data});
+      this.setState({ tacheList: data});
     }.bind(this))
     .fail(function(jqXHR, textStatus, errorThrown) 
     {
@@ -49,12 +49,12 @@ class App extends React.Component {
 
   render() {
    
-    let tasksArrayMap = this.state.taskList.map((task, i) => {
+    let tachesArrayMap = this.state.tacheList.map((tache, i) => {
       return (
-        <Task 
+        <Tache 
           key={i}
-          value={task.value}
-          done={task.done}
+          value={tache.value}
+          done={tache.done}
         />
       )
     })
@@ -63,12 +63,12 @@ class App extends React.Component {
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <h1> Tâches à faire</h1>
+            <h1> Mes taches</h1>
             <form
               id="form-add"
               className="form-horizontal">
               <div className="input-group">
-                <input type="text" id="addInput" className="form-control"  placeholder="Description de la tâche..." />
+                <input type="text" id="addInput" className="form-control"  placeholder="Enterz une tache..." />
                 <div className="input-group-btn">
                   <button type="submit" className="btn btn-default">
                     <span className="glyphicon glyphicon-plus-sign"></span>
@@ -77,7 +77,7 @@ class App extends React.Component {
               </div>
             </form>
 
-            {tasksArrayMap}
+            {tachesArrayMap}
             
           </div>
         </div>
