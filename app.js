bb5f3b8c2a1e3bc6aa4b9c2d51a920d0d556363c
dahrif <dahri.fatima.dev@gmail.com>
2021-06-25 12:54:39 +0100

    done?

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 // Composant : Tâche
 class Tache extends React.Component {
-   
+
 
   render() {
-      let class_name = 'tache'
-      class_name += this.props.done ? ' tache-faite' : ' tache-info';
+    let class_name = 'tache'
+    class_name += this.props.done == 1 ? ' tache-faite' : ' tache-info';
 
-      return (
-          <div className={class_name}>
-              <span>{this.props.value}</span>
-              <i className="close" onClick={this.props.onClickClose}>&times;</i>
-          </div>
-      )
+    return (
+      <div className={class_name} onClick={this.props.onClickTask}>
+        <span>{this.props.value}</span>
+        <i className="close" onClick={this.props.onClickClose}>&times;</i>
+      </div>
+    )
   }
 }
 
@@ -22,90 +22,108 @@ class App extends React.Component {
     super(props)
 
     this.state = {
-       tacheList: []
+      tacheList: []
     };
   }
   componentDidMount() {
     this.chargementDonnees();
   }
-  chargementDonnees(){
+  chargementDonnees() {
 
     var dataList = null;
-    
+
     // affichage de données par Ajax
 
-    $.getJSON( "api/showtasks.php", 
-    function( data ) {
-      this.setState({ tacheList: data});
-    }.bind(this))
-    .fail(function(jqXHR, textStatus, errorThrown) 
-    {
-       console.log(errorThrown);
-   })
-    ;
- 
+    $.getJSON("api/showtasks.php",
+      function (data) {
+        this.setState({ tacheList: data });
+      }.bind(this))
+      .fail(function (jqXHR, textStatus, errorThrown) {
+        console.log(errorThrown);
+      })
+      ;
+
   }
 
   addTask(e) {
 
     $.ajax({
-      url:"/api/addtask.php",
-      method:"POST",
-      data:{
-          taskname : addInput.value ,
+      url: "/api/addtask.php",
+      method: "POST",
+      data: {
+        taskname: addInput.value,
       },
-      success:function(data) {
+      success: function (data) {
         this.chargementDonnees()
         console.log(data)
-    }.bind(this)
+      }.bind(this)
     })
-   e.preventDefault()
- }
+    e.preventDefault()
+  }
 
- removeTache(i) {
+  removeTache(i) {
 
     $.ajax({
-    url:"/api/deletetask.php",
-    method:"POST",
-    data:{
-      sid: i
-    },
-    success:function(data) {
-      $(this).parent().remove();
-      this.chargementDonnees()
-    }.bind(this)
-  })
+      url: "/api/deletetask.php",
+      method: "POST",
+      data: {
+        sid: i
+      },
+      success: function (data) {
+        $(this).parent().remove();
+        this.chargementDonnees()
+      }.bind(this)
+    })
 
-}
+  }
 
-markDone(i) {
-  // let tasksArray = this.state.tasksArray
-  // let task = this.state.tasksArray[i]
-  // tasksArray.splice(i, 1)
-  // task.done = !task.done 
+  markDone(i , status) {
+
+        if(status !=1){
+          $.ajax({
+            url:"api/done.php",
+            method:"POST",
+            data:{
+              sid : i,
+              done : 1,
+            },
+                success:function(data) {
+                  console.log(data)
+                  this.chargementDonnees()
+                }.bind(this)
+          })
+        }else{
+            $.ajax({
+              url:"api/done.php",
+              method:"POST",
+              data:{
+                sid : i,
+                done : 0,
+              },
+                  success:function(data) {
+                    console.log(data)
+                    this.chargementDonnees()
+                  }.bind(this)
+            })
+          }
+        }
+
+
+
+
   
-  // task.done ? tasksArray.push(task) : tasksArray.unshift(task)
-
-
-  // this.setState({
-  //   tasksArray: tasksArray
-  // })
-
-  
-
-  
-}
 
 
   render() {
-   
+
     let tachesArrayMap = this.state.tacheList.map((tache) => {
       return (
-        <Tache 
+        <Tache
           key={tache.idtasks}
           value={tache.taskname}
           done={tache.done}
           onClickClose={this.removeTache.bind(this, tache.idtasks)}
+          onClickTask={this.markDone.bind(this, tache.idtasks ,tache.done)}
         />
       )
     })
@@ -119,7 +137,7 @@ markDone(i) {
               id="form-add"
               className="form-horizontal" onSubmit={this.addTask.bind(this)}>
               <div className="input-group">
-                <input type="text" id="addInput" className="form-control"  placeholder="Enterz une tache..." />
+                <input type="text" id="addInput" className="form-control" placeholder="Enterz une tache..." />
                 <div className="input-group-btn">
                   <button type="submit" className="btn btn-default">
                     <span className="glyphicon glyphicon-plus-sign"></span>
@@ -129,7 +147,7 @@ markDone(i) {
             </form>
 
             {tachesArrayMap}
-                        
+
           </div>
         </div>
       </div>
